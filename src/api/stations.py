a96f5b48d9da57cb4e5b0e5e00576a629d645253
Beniamin <beniamin.mon@gmail.com>
2023-11-19 14:10:24 +0100

    fix : route stations must end by /

--- src/api/stations.py
+++ src/api/stations.py
@@ -3,7 +3,7 @@ from services.stations_service import get_stations_service
 stations_route = Blueprint('stations_route', __name__)
 
 
-@stations_route.route("/api/v1/stations", methods=['GET'])
+@stations_route.route("/api/v1/stations/", methods=['GET'])
 def get_stations():
     args = request.args
     type=args.get("type", default="", type=str)
