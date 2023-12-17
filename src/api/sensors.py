352180f9e936b9072f988a870e8a38b23963a34b
cballevre <celestin.ballevre@protonmail.com>
2023-12-17 20:54:19 +0100

    fix: Database schema to adapt prisma generated one

--- src/api/sensors.py
+++ src/api/sensors.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request
 from services.sensors_service import get_sensors_service
 sensors_route = Blueprint('sensors_route', __name__)
 
-
 @sensors_route.route("/api/v1/stations/<path:id_station>/sensors", methods=['GET'])
 def get_sensors(id_station):
     return get_sensors_service(id_station)
--- src/model/sensor.py
+++ src/model/sensor.py
@@ -1,7 +1,15 @@
-from mongoengine import Document, StringField,IntField
+from mongoengine import Document, StringField, IntField, DateTimeField, ObjectIdField
 
 
 class sensors(Document):
     identifier = StringField(required=True)
+    type = StringField(required=True)
     nbr_measures = IntField(required=True)
-    station_id = StringField(required=True)
+    station_id = ObjectIdField(required=True)
+    created_at = DateTimeField(required=True)
+    created_by_id = StringField(required=False)
+    updated_at = DateTimeField(required=True)
+    updated_by_id = StringField(required=False)
+    deleted_at = DateTimeField(required=False)
+    deleted_by_id = StringField(required=False)
+    deleted = StringField(required=False)
